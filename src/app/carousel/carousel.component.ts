c9882d4033741beb91052eaccf586a737eaaba77
Bahaabrougui <bouhaa7@gmail.com>
2019-04-24 11:27:27 +0100

    cmt

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -16,7 +16,7 @@ export class CarouselComponent implements OnInit {
   constructor(config: NgbCarouselConfig) {
     // customize default values of carousels used by this component tree
     config.interval = 10000;
-    config.wrap = false;
+    config.wrap = true;
     config.keyboard = false;
 
     config.showNavigationArrows = true;
