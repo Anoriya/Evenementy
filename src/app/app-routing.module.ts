7b923b7bac4012253ff4f58e87e64c3c85de110d
Bahaabrougui <bouhaa7@gmail.com>
2019-04-24 11:50:13 +0100

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { CarouselComponent} from 'src/app/carousel/carousel.component';
 import {LoginComponent} from './login/login.component';
 import {RegisterComponent} from './register/register.component';
+import {SearchComponent} from './search/search.component';
 
 const routes: Routes = [
 
   { path: '', component: CarouselComponent  },
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent }
+  { path: 'register', component: RegisterComponent },
+  { path: 'search', component : SearchComponent}
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
